ca6555ff929d67d824c0c26174c4502a3707f8d9
Jacob Williams <jacobaw@gmail.com>
2019-10-26 15:57:31 -0500

    bundle gem trello_backup_renderer

---
+++ lib/trello_backup_renderer.rb
@@ -0,0 +1,6 @@
+require "trello_backup_renderer/version"
+
+module TrelloBackupRenderer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/trello_backup_renderer/version.rb
@@ -0,0 +1,3 @@
+module TrelloBackupRenderer
+  VERSION = "0.1.0"
+end
